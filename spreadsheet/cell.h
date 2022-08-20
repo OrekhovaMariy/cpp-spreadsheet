dcdd1ebbd96edd610438a7a2ef6c2c008f527065
Mariya <orekhovamariy@yandex.ru>
2022-08-20 19:48:34 +0300

    16 спринт
    16 спринт

--- spreadsheet/cell.h
+++ spreadsheet/cell.h
@@ -3,34 +3,72 @@
 #include "common.h"
 #include "formula.h"
 
-#include <functional>
+#include <optional>
 #include <unordered_set>
 
 class Sheet;
 
-class Cell : public CellInterface {
+class Cell final : public CellInterface {
 public:
     Cell(Sheet& sheet);
     ~Cell();
 
-    void Set(std::string text);
+    void Set(const std::string& text);
     void Clear();
 
     Value GetValue() const override;
     std::string GetText() const override;
+
     std::vector<Position> GetReferencedCells() const override;
 
-    bool IsReferenced() const;
+    void FindCyclicDependencies(std::vector<Position> cells);
+
+    void SetDependentCell(Cell* cell);
 
 private:
     class Impl;
     class EmptyImpl;
     class TextImpl;
     class FormulaImpl;
-
     std::unique_ptr<Impl> impl_;
+    Sheet& sheet_;
+    std::unordered_set<Cell*> all_dependent_cells_ = {};
+};
 
-    // Добавьте поля и методы для связи с таблицей, проверки циклических 
-    // зависимостей, графа зависимостей и т. д.
+class Cell::Impl {
+public:
+    Impl() = default;
+    virtual Value GetValue() const = 0;
+    virtual std::string GetText() const = 0;
+    virtual std::vector<Position> GetReferencedCells() const = 0;
+};
 
+class Cell::EmptyImpl final : public Impl {
+public:
+    EmptyImpl() = default;
+    CellInterface::Value GetValue() const override;
+    std::string GetText() const override;
+    std::vector<Position> GetReferencedCells() const override;
+};
+
+class Cell::TextImpl final : public Impl {
+public:
+    TextImpl(std::string text);
+    CellInterface::Value GetValue() const override;
+    std::string GetText() const override;
+    std::vector<Position> GetReferencedCells() const override;
+private:
+    std::string text_;
+};
+
+class Cell::FormulaImpl final : public Impl {
+public:
+    explicit FormulaImpl(std::string text_formula, const SheetInterface& sheet);
+    CellInterface::Value GetValue() const override;
+    std::string GetText() const override;
+    std::vector<Position> GetReferencedCells() const override;
+private:
+    std::unique_ptr<FormulaInterface> formula_;
+    mutable std::optional<FormulaInterface::Value> cache_;
+    const SheetInterface& sheet_;
 };
