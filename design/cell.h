948cc5cfdca92c6529b1f0376820a7e80179b38e
Mariya <orekhovamariy@yandex.ru>
2022-08-20 21:57:40 +0300

    16 спринт
    16 спринт

--- design/cell.h
+++ design/cell.h
@@ -3,16 +3,27 @@
 #include "common.h"
 #include "formula.h"
 
-class Cell : public CellInterface {
+#include <optional>
+#include <unordered_set>
+
+class Sheet;
+
+class Cell final : public CellInterface {
 public:
-    Cell();
+    Cell(Sheet& sheet);
     ~Cell();
 
-    void Set(std::string text);
+    void Set(const std::string& text);
     void Clear();
 
     Value GetValue() const override;
     std::string GetText() const override;
+
+    std::vector<Position> GetReferencedCells() const override;
+
+    void FindCyclicDependencies(std::vector<Position> cells);
+
+    void SetDependentCell(Cell* cell);
 
 private:
     class Impl;
@@ -20,4 +31,44 @@ private:
     class TextImpl;
     class FormulaImpl;
     std::unique_ptr<Impl> impl_;
+    Sheet& sheet_;
+    std::unordered_set<Cell*> all_dependent_cells_ = {};
+};
+
+class Cell::Impl {
+public:
+    Impl() = default;
+    virtual Value GetValue() const = 0;
+    virtual std::string GetText() const = 0;
+    virtual std::vector<Position> GetReferencedCells() const = 0;
+};
+
+class Cell::EmptyImpl final : public Impl {
+public:
+    EmptyImpl() = default;
+    CellInterface::Value GetValue() const override;
+    std::string GetText() const override;
+    std::vector<Position> GetReferencedCells() const override;
+};
+
+class Cell::TextImpl final : public Impl {
+public:
+    TextImpl(std::string text);
+    CellInterface::Value GetValue() const override;
+    std::string GetText() const override;
+    std::vector<Position> GetReferencedCells() const override;
+private:
+    std::string text_;
+};
+
+class Cell::FormulaImpl final : public Impl {
+public:
+    explicit FormulaImpl(std::string text_formula, const SheetInterface& sheet);
+    CellInterface::Value GetValue() const override;
+    std::string GetText() const override;
+    std::vector<Position> GetReferencedCells() const override;
+private:
+    std::unique_ptr<FormulaInterface> formula_;
+    mutable std::optional<FormulaInterface::Value> cache_;
+    const SheetInterface& sheet_;
 };
